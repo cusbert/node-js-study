999e76b0aa0296305f02f415e0f965dfa02ca4f3
cusbert <mindychoi20@gmail.com>
2021-05-14 00:12:22 +0900

    express 미들웨어 실습 중

--- ch6/learn-express/app.js
+++ ch6/learn-express/app.js
@@ -1,15 +1,34 @@
-const { RSA_NO_PADDING } = require('constants');
 const express = require('express');
 const path = require('path');
-
-/**
+const morgan = require('morgan'); // 요청에 대한 로그를 콘솔에 출력해줌
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv'); // 환경 변수 읽어들임
+/** /
  *  미들웨어 : 요청과 응답의 중간에 위치 
  *  app.use(미들웨어)
  * 
  */
-
+dotenv.config(); // dotenv 가 환경변수 읽어서 process.env 생성 -> ex) GET / 500 2.333 ms - 43
 const app = express(); // express 모듈 할당
 app.set('port', process.env.PORT || 3000); // express 포트 설정
+
+app.use(morgan('dev')); // dev, combined, common, short 등 넣을수 있음
+app.use('/', express.static(path.join(__dirname, 'public'))); // static 은 정적파일 제공하는 라우터
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+    name: 'session-cookie'
+}));
 
 
 app.use((req, res, next) => { // 주소 없음 -> 모든 요청에 대해 실행
